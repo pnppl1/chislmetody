5a3d92f692c2c5d1b751df0ce3de908c445973fe
pnppl1 <47748417+pnppl1@users.noreply.github.com>
2019-02-18 19:44:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  a, q, x;
+            double e = Math.Pow(10, -6);
+            x = 0;
+            a = Math.Pow(-1, 0) * Math.Pow(Math.PI / 2, 2 * 0) * Math.Pow(x, 4 * 0 + 1);
+            for (int n = 0; (a > e) && (x < 1.5); n++)
+            {
+                q = -Math.Pow(Math.PI / 2, 2) * Math.Pow(x, 4) * (4 * n + 1) / ((2 * n + 1) * (2 * n + 2) * (4 * n + 5));
+                a = a * q;
+                x = x + n * 0.15;
+                Console.WriteLine(Convert.ToString(x), a);
+            }
+        }
+    }
+}
